38d0f28cdf480070c25c4f4023e06b0be8cc0ac6
anubhav bisht <anubhavbisht98@gmail.com>
2024-11-12 09:37:53 +0530

    learnt about validationand pipes.Learnt about default validation pipes,dtos,class validator and class transformer

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,26 +7,42 @@ import {
   Body,
   Headers,
   Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
+  Patch,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-params.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  public getUsers(
+    @Param() getUserParamDto: GetUsersParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(getUserParamDto, typeof getUserParamDto);
+    console.log(limit, typeof limit);
+    console.log(page, typeof page);
     return 'Hello from get users endpoint';
   }
 
   @Post()
   public createUsers(
-    @Body() request: any,
+    @Body() createUserDto: CreateUserDto,
     @Headers() headers: any,
     @Ip() ip: any,
   ) {
-    console.log(request);
+    console.log(createUserDto);
     console.log(headers);
     console.log(ip);
     return 'Hello from post users endpoint';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
   }
 }
